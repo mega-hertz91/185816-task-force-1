960e7bd2a0a983fd74fe5453d5cfb58f87e1c2d8
Бабушкин Игорь <ibabushkin@elewise.local>
2019-12-30 11:52:18 +0400

    Добавляет модели и выводит страницу с юзерами

---
+++ frontend/models/Category.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property int $id
+ * @property string|null $category_name
+ * @property string|null $tag
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property Task[] $tasks
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['created_at', 'updated_at'], 'safe'],
+            [['category_name', 'tag'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'category_name' => 'Category Name',
+            'tag' => 'Tag',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTasks()
+    {
+        return $this->hasMany(Task::className(), ['category_id' => 'id']);
+    }
+}
---
+++ frontend/models/Response.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "response".
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property int|null $amount
+ * @property int|null $task_id
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property User $user
+ * @property Task $task
+ */
+class Response extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'response';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'amount', 'task_id'], 'integer'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+            [['task_id'], 'exist', 'skipOnError' => true, 'targetClass' => Task::className(), 'targetAttribute' => ['task_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'amount' => 'Amount',
+            'task_id' => 'Task ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTask()
+    {
+        return $this->hasOne(Task::className(), ['id' => 'task_id']);
+    }
+}
