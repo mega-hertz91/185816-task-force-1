5963764c43b66d0309eefee36ab7b8d9b5457268
Бабушкин Игорь <ibabushkin@elewise.local>
2020-03-16 14:46:43 +0400

    Добавляет таблицу

---
+++ console/migrations/m200316_080541_create_category_executor_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%category_executor}}`.
+ */
+class m200316_080541_create_category_executor_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%category_executor}}', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'category_id' =>$this->integer()->notNull()
+        ]);
+
+        $this->addForeignKey('fke-user-executor', 'category_executor', 'user_id', 'user', 'id' );
+        $this->addForeignKey('fke-category-executor', 'category_executor', 'category_id', 'category', 'id' );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%category_executor}}');
+    }
+}
